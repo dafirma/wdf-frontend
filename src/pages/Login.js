20b690b2711305197192e2f54134f79f1a95d2de
Hugo Resende <dafirma@gmail.com>
2019-05-23 00:32:56 +0200

    fixed web button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,57 +40,81 @@ class Login extends Component {
   render() {
     const { username, password } = this.state;
     const {formErrors} = this.state;
-   // console.log(this.props)
-    
-    //{console.log(this.props.user)}
 
     return (
-    
-      <div className='parent-login'>
-      <div className='login'>
-        <div className='container-login'>
-          <div className='container-logo'>
-            <img src = {logo} alt='logo-wdf'/>
-          </div> 
-          <form className='form-login'onSubmit={this.handleFormSubmit}>
-        <div className='container-main-input'>
-        <div className='container-input-login'> 
-        {formErrors && 
-        <div><h2>Error, fields empty!</h2></div> }
-        <label></label>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={this.handleChange}
-          placeholder="Username"
-        />
-      </div>
-        <div className='container-input-login'>
+      <div className='container-signup'>
+      <div className='container-logo-signup'>
+          <img src = {logo} alt='logo-wdf'/>
+      </div> 
+      {formErrors && 
+      <div><h2>Error, fields empty!</h2></div> }
+      <form className='ui-form-signup' onSubmit={this.handleFormSubmit}>
+        <div className='container-input-signup'>
+          <div className='container-child-searchbar'>
+            <div className='container-input-signup'>
+            <input
+              onChange={this.handleChange}
+              value={username}
+              name='username'
+              type='text'
+              className='centerInput'
+              placeholder='Username'
+            />
+            </div>
+            <div className='container-input-signup'>
+              <input
+                onChange={this.handleChange}
+                value={password}
+                name='password'
+                type='password'
+                className='centerInput'
+                placeholder='Password'
+              />
 
-        <label></label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-          placeholder="Password"
-        />
-        </div>
-        <div className='container-btn-login'>
-        <button className='btn-login' type="submit"><p>Login</p></button>
-        </div>
+            </div>
+            
+          </div>
         
+        <div className='container-btn-signup'>
+          <button className='btn-signup'type='submit'><p>Login</p></button>
         </div>
-        
+      </div>
       </form>
-     <li> To create account <Link to="/signup"><span className='text-to-signup'>Sign Up</span></Link></li>
-     </div>
-    </div>
-    </div>
+            <li> To create account <Link to="/signup"><span className='text-to-signup'>Sign Up</span></Link></li>
+            </div>
       
     );
   }
 }
 
 export default withAuth(Login);
+
+/* 
+
+<div className='container-main-input'>
+              <div className='container-input-login'> 
+                {formErrors && 
+                <div><h2>Error, fields empty!</h2></div> }
+                <label></label>
+                <input
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={this.handleChange}
+                  placeholder="Username"
+                />
+              </div>
+              <div className='container-input-login'>
+                <label></label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={this.handleChange}
+                  placeholder="Password"
+                />
+                </div>
+                <div className='container-btn-login'>
+                 <button className='btn-login' type="submit"><p>Login</p></button>
+                </div>
+                </div> */
